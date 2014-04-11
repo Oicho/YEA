76edea5e066d2a2c8fc1ef4fb4707f67ff490bf9
Fridiere Gauthier <gauthier.fridiere@epita.fr>
2014-04-11 20:16:58 +0200

    scoped map set up

--- src/scoped_map.ml
+++ src/scoped_map.ml
@@ -1,20 +1,36 @@
 open Typer;;
+exception No_scope_started;;
+
 class scoped_map =
   object (self)
   val  mutable  map : (string, number) Hashtbl.t list = []
   method scope_begin = map <- (Hashtbl.create 127542)::map
+
   method scope_end = match map with
     | e::l -> map <- l
-    | _ -> failwith("no scope started")
+    | _ -> raise No_scope_started
+
   method set_var name value  = match map with
   | e::l -> Hashtbl.add e name value
-  | _ -> failwith("no scope started")
-  method get_var name = match map with
-  | e::l -> begin
+  | _ -> raise No_scope_started
+
+  method get_var name =
+    let rec f_get_var name  = function
+      | e::l ->
+       begin
+        try
+          (Hashtbl.find e name)
+        with
+        | Not_found -> f_get_var name l
+      end
+      | _ ->  raise Not_found
+    in
     try
-      (Hashtbl.find e name)
+      match map with
+      | [] -> raise No_scope_started
+      | _ -> f_get_var name map
     with
-      | Not_found -> failwith("Not_found")
-  end
-  | _ -> failwith("no scope started")
-  end
+    | Not_found-> match map with
+    | e::l -> (Hashtbl.add e name (Int(0))); Int(0)
+    | _ -> raise No_scope_started
+end
