61c584fe01d7eff2dde4c65afa0eac2e220e5b4a
Gauthier Fridiere <gauthier.fridiere@gmail.com>
2014-05-16 12:41:44 +0200

    Reset the working main

--- src/main.ml
+++ src/main.ml
@@ -9,20 +9,17 @@ let main_loop lexbuf =
    while true do
       let result = Parser.instruction Lexer.lex lexbuf in
         (*
-        Calc_visitor.scan_ast result;
-      *)
         Print_visitor.scan_ast result;
+      *)
+        Calc_visitor.scan_ast result;
     done
 
 let _ =
   try
     let lexbuf = Lexing.from_channel stdin in
-    (*if pretty print*)
-    print_string  "graph ast {\n  base [label=\"Program\"];\n";
     var_map#scope_begin;
     main_loop lexbuf;
    
     
-  with Lexer.Eof ->
+  with Lexer.Eof -> exit 0
   (*if pretty print*)
-    print_string "\n}"
